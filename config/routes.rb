51147b2cc8fe7337363f17cff379ba1272fc0ecb
joedietrich-dev <joe@joedietrich.net>
2022-01-17 21:10:08 -0500

    Set up for Heroku release

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end
