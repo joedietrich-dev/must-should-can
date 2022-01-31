6e4e9c769adc164c6ed1bd30d532aa4dbd79d4f4
joedietrich-dev <joe@joedietrich.net>
2022-01-30 22:16:29 -0500

    Stub out tasks

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,6 @@
 class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:email])
-    p user
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
       render json: user, status: :created
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,16 @@
 class TasksController < ApplicationController
+  before_action :authorize
+
   # GET /tasks
   def index
-    @tasks = Task.all
-
+    @user = User.find(session[:user_id])
+    @tasks = Task.find_by(user: @user)
+    puts @user
     render json: @tasks
+  end
+
+  private
+  def authorize
+    return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
   end
 end
