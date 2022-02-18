3d969bc84b19ccd69b50a0be3e45f2ee8937b6fa
joedietrich-dev <joe@joedietrich.net>
2022-02-17 20:21:53 -0500

    Add padding to bottom of page

--- client/src/components/common/Main.jsx
+++ client/src/components/common/Main.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const Main = styled.main`
   max-width: 1200px;
   width: 95vw;
-  margin: 0 auto;
+  margin: 0 auto 1rem auto;
   box-sizing: content-box;
 `;
 
